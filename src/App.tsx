7a871c6550b841de6e2982b479a1384ae6edb8fa
Wangleng Tan <wangleng.tan@shopee.com>
2023-08-07 15:36:19 +0800

    Add minimum reproduction

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
-function App() {
-  return <div>Hello world!</div>
-}
+import { Form } from "@rjsf/antd";
+import { RJSFSchema, UiSchema } from "@rjsf/utils";
+import validator from "@rjsf/validator-ajv8";
 
-export default App
+const SCHEMA: RJSFSchema = {
+  title: "Bug Report",
+  type: "object",
+  properties: {
+    multipleChoicesList: {
+      type: "array",
+      title: "A multiple choices list",
+      items: {
+        type: "string",
+        enum: ["foo", "bar", "fuzz", "qux"],
+      },
+      uniqueItems: true,
+    },
+  },
+};
+
+const UI_SCHEMA: UiSchema = {
+  multipleChoicesList: {
+    "ui:widget": "checkboxes",
+  },
+};
+
+export default function App() {
+  return <Form schema={SCHEMA} uiSchema={UI_SCHEMA} validator={validator} />;
+}
